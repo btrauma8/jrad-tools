00c3274fdbbc6b21f5f6c1a1a50149717ebcd1a9
btrauma8 <btrauma8@yahoo.com>
2020-09-24 06:53:02 -0500

    css grid label value

--- src/components/css-grid/css-grid.tsx
+++ src/components/css-grid/css-grid.tsx
@@ -19,3 +19,20 @@ export const CssGrid = ({ cols, css, children, repeatSize="1fr", ...props }:CssG
 
     return <div className={_css} style={gridStyle}>{ children }</div>
 }
+
+export const TableLikeCssGrid = ({ children, ...props }:CssGridProps) => {
+    return <CssGrid justifyContent="flex-start" alignItems="center" {...props}>{ children }</CssGrid>
+}
+
+export const LabelValueGrid = ({ children }:any) => {
+    // ALWAYS two columns
+    const gridStyle:React.CSSProperties = {
+        display: 'grid',
+        columnGap: 'var(--sp-1)',
+        rowGap: 'var(--sp-1)',
+        gridTemplateColumns: 'auto 1fr',
+        justifyContent: 'flex-start',
+        alignItems: 'center'
+    };
+    return <div style={gridStyle}>{ children }</div>;
+}
