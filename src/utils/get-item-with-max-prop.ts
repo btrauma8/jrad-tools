bb2551d8696414b200bc027dc64c7dc56de0e0c0
btrauma8 <btrauma8@yahoo.com>
2020-12-07 09:04:04 -0600

    get min item

--- src/utils/get-item-with-max-prop.ts
+++ src/utils/get-item-with-max-prop.ts
@@ -16,3 +16,22 @@ export const getItemWithMaxProp = <T>(items:T[], fn:(x:T) => number|undefined):T
     }, undefined)
     return itemWithMaxProperty;
 }
+
+export const getItemWithMinProp = <T>(items:T[], fn:(x:T) => number|undefined):T|undefined => {
+    // array of T, wanna find the one with min T.X, use this.
+    // (if T.X is undefined, cannot be considered)
+    // (if no valid values found, returns undefiend)
+    const itemWithMinProperty:T|undefined = items.reduce((prev:T|undefined, curr:T|undefined) => {
+        const prevVal = prev ? fn(prev) : undefined;
+        const currVal = curr ? fn(curr) : undefined;
+        if (prevVal === undefined) {
+            return curr;
+        } else if (currVal === undefined) {
+            return prev;
+        } else {
+            if (prevVal < currVal) return prev;
+            return curr;
+        }
+    }, undefined)
+    return itemWithMinProperty;
+}
