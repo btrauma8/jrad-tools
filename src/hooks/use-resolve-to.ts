e5b9eec22ab4348a2de204a9eb95ff2acc040066
btrauma8 <btrauma8@yahoo.com>
2020-12-26 14:36:41 -0600

    clean up

--- src/hooks/use-resolve-to.ts
+++ src/hooks/use-resolve-to.ts
@@ -2,7 +2,6 @@ import { useResolvedPath } from 'react-router-dom';
 import { getMergedPath } from './route-helper';
 
 export const useResolveTo = (to:string) => {
-
     // First, where are we currently?
     const relBasePath = useResolvedPath('').pathname;
     // If we determine this "to" is relative (../har or har), use this path
@@ -10,21 +9,17 @@ export const useResolveTo = (to:string) => {
 
     if (relBasePath === '') {
         // NOT within base <Routes />, so, we have to apply our basePath
-        console.log('useResolveTo', to, 'relBasePath', relBasePath, 'resolvedPathIfRelative', resolvedPathIfRelative, 'abs1', getMergedPath(to));
+        // console.log('useResolveTo', to, 'relBasePath', relBasePath, 'resolvedPathIfRelative', resolvedPathIfRelative, 'abs1', getMergedPath(to));
         return getMergedPath(to); // absolute path
     } else {
         // we are within /something, so ...
         // ...unless they are giving us an absolute path (starts with /) just let it fly as-is
         if (to.startsWith("/")) {
-            console.log('useResolveTo', to, 'relBasePath', relBasePath, 'resolvedPathIfRelative', resolvedPathIfRelative, 'abs2', getMergedPath(to));
+            // console.log('useResolveTo', to, 'relBasePath', relBasePath, 'resolvedPathIfRelative', resolvedPathIfRelative, 'abs2', getMergedPath(to));
             return getMergedPath(to);  // absolute path
         } else {
-            console.log('useResolveTo', to, 'relBasePath', relBasePath, 'resolvedPathIfRelative', resolvedPathIfRelative, 'rel');
+            // console.log('useResolveTo', to, 'relBasePath', relBasePath, 'resolvedPathIfRelative', resolvedPathIfRelative, 'rel');
             return resolvedPathIfRelative; // it was already relative. do nothing.
         }
     }
-
-    // First attempt...resolving the actual "to" that they sent. Don't do this.
-    // const path = useResolvedPath(to).pathname;
-    // return getMergedPath(path);
 }
