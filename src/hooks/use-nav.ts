ff642263a1ee08b17cc0c19a55840c59149ea723
btrauma8 <btrauma8@yahoo.com>
2020-12-26 14:27:19 -0600

    more nav

--- src/hooks/use-nav.ts
+++ src/hooks/use-nav.ts
@@ -1,29 +1,12 @@
 import { useNavigate } from 'react-router-dom';
-import { useResolveTo } from './use-resolve-to';
-// import { getMergedPath } from './route-helper';
-import { getMergedPath, getRouterBasePath } from './route-helper';
-import { resolvePath, useResolvedPath } from 'react-router-dom';
+// import { useResolveTo } from './use-resolve-to';
+import { getMergedPath } from './route-helper';
+import { useResolvedPath } from 'react-router-dom';
 
 export const useNav = () => {
     const nav = useNavigate();
-    // resolvePath
-    // basePath: "/bp2/"
-
-    /*
-        basePath: /bp2/
-        useNav('admin')
-    */
     const relBasePath = useResolvedPath('').pathname; // will return "/" if you are root
-
     return (to:string) => {
-        // current path: "/admin", actually "/bp2/admin"
-        // path: "../poop"
-        // we must return "/bp2/poop"
-
-        // console.log(resolvePath())
-
-        // const toPath = resolvePath(to, getRouterBasePath());
-
         if (relBasePath === '') {
             // NOT within base <Routes />, so, we have to apply our basePath
             nav(getMergedPath(to));
@@ -36,6 +19,5 @@ export const useNav = () => {
                 nav(to); // it was already relative. do nothing.
             }
         }
-        // nav(useResolveTo(to));
     }
 }
--- src/hooks/use-resolve-to.ts
+++ src/hooks/use-resolve-to.ts
@@ -2,6 +2,29 @@ import { useResolvedPath } from 'react-router-dom';
 import { getMergedPath } from './route-helper';
 
 export const useResolveTo = (to:string) => {
-    const path = useResolvedPath(to).pathname;
-    return getMergedPath(path);
+
+    // First, where are we currently?
+    const relBasePath = useResolvedPath('').pathname;
+    // If we determine this "to" is relative (../har or har), use this path
+    const resolvedPathIfRelative = useResolvedPath(to).pathname;
+
+    if (relBasePath === '') {
+        // NOT within base <Routes />, so, we have to apply our basePath
+        console.log('useResolveTo', to, 'relBasePath', relBasePath, 'resolvedPathIfRelative', resolvedPathIfRelative, 'abs1', getMergedPath(to));
+        return getMergedPath(to); // absolute path
+    } else {
+        // we are within /something, so ...
+        // ...unless they are giving us an absolute path (starts with /) just let it fly as-is
+        if (to.startsWith("/")) {
+            console.log('useResolveTo', to, 'relBasePath', relBasePath, 'resolvedPathIfRelative', resolvedPathIfRelative, 'abs2', getMergedPath(to));
+            return getMergedPath(to);  // absolute path
+        } else {
+            console.log('useResolveTo', to, 'relBasePath', relBasePath, 'resolvedPathIfRelative', resolvedPathIfRelative, 'rel');
+            return resolvedPathIfRelative; // it was already relative. do nothing.
+        }
+    }
+
+    // First attempt...resolving the actual "to" that they sent. Don't do this.
+    // const path = useResolvedPath(to).pathname;
+    // return getMergedPath(path);
 }
