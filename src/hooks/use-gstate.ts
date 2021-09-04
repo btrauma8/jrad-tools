1014e314982ddb029ffcc124b846d030a92e6bd2
btrauma8 <btrauma8@yahoo.com>
2021-09-04 15:38:38 -0500

    gstate

--- src/hooks/use-gstate.ts
+++ src/hooks/use-gstate.ts
@@ -1,15 +1,39 @@
 import { useState, useEffect } from 'react';
 import { BehaviorSubject } from 'rxjs';
 
+// super simple cross-component state util.
+
 const states = new Map<string, BehaviorSubject<any>>();
 
-export const setGState = <T>(key:string, x:T) => states.get(key)?.next(x);
+export const mergeGState = <T>(key:string, patch:Partial<T>) => {
+    const st = getGState<T>(key);
+    setGState(key, {
+        ...st,
+        ...patch
+    })
+}
 
-export const useGState = <T>(key:string, initVal:T):[T, (x:T) => void] => {
+export const getGState = <T>(key:string) => states.get(key)?.getValue() as T;
 
-    // very simple hook where suddenly you can have simple cross-component state
+export const setGState = <T>(key:string, x:T) => {
+    if (!states.has(key)) {
+        states.set(key, new BehaviorSubject(x));
+    } else {
+        states.get(key)!.next(x);
+    }
+}
 
-    if (!states.has(key)) states.set(key, new BehaviorSubject<T>(initVal));
+export const useGState = <T>(key:string, initVal?:T | (() => T)):[T, (x:T) => void] => {
+
+    if (!states.has(key)) {
+        if (!initVal) throw new Error('You must ensure useGState provides an initVal prior to calling it without initval');
+        if (typeof initVal === 'function') {
+            states.set(key, new BehaviorSubject<T>((initVal as ()=>T)()));
+        } else {
+            states.set(key, new BehaviorSubject<T>(initVal));
+        }
+        
+    }
 
     const [ val, _setVal ] = useState<T>(states.get(key)!.getValue());
 
