8ad5811b88aa0ee3d02c8071f9868a018bfff868
btrauma8 <btrauma8@yahoo.com>
2021-11-23 17:55:08 -0600

    use outside click

--- src/hooks/use-on-click-outside.ts
+++ src/hooks/use-on-click-outside.ts
@@ -1,5 +1,34 @@
 import * as React from 'react';
 
+// export const useOnClickOutside = (elem:HTMLElement|null, handler:(evt:MouseEvent | TouchEvent) => void) => {
+
+//     // use layout effect --- very important.
+//     // if you use normal useEffect, it'll jump in front of things like react dropdown's onchange ... 
+//     // and you'll end up closing whatever thing is open and not getting the change value,
+//     // or you'll overwrite it wth an old value, etc...
+//     // it's a lifecycle thing.
+
+//     React.useLayoutEffect(() => {
+//         // console.log('ref or handler changed');
+//         const listener = (event:MouseEvent | TouchEvent) => {
+
+//             // Do nothing if clicking ref's element or descendent elements
+//             if (!elem || (elem as any).contains(event.target)) {
+//                 return;
+//             }
+//             // console.log('we heard this', event);
+//             handler(event);
+//         }  
+//         document.addEventListener('mousedown', listener, true); // capturing phase so we can preventDefault it
+//         document.addEventListener('touchstart', listener);
+//         return () => {
+//             document.removeEventListener('mousedown', listener);
+//             document.removeEventListener('touchstart', listener);
+//         }
+//     }, [elem, handler])
+// }
+
+
 export const useOnClickOutside = (elem:HTMLElement|null, handler:(evt:MouseEvent | TouchEvent) => void) => {
 
     // use layout effect --- very important.
@@ -9,21 +38,17 @@ export const useOnClickOutside = (elem:HTMLElement|null, handler:(evt:MouseEvent
     // it's a lifecycle thing.
 
     React.useLayoutEffect(() => {
-        // console.log('ref or handler changed');
-        const listener = (event:MouseEvent | TouchEvent) => {
-
+        const listener = (event:MouseEvent) => {
             // Do nothing if clicking ref's element or descendent elements
             if (!elem || (elem as any).contains(event.target)) {
                 return;
             }
-            // console.log('we heard this', event);
             handler(event);
-        }  
-        document.addEventListener('mousedown', listener, true); // capturing phase so we can preventDefault it
-        document.addEventListener('touchstart', listener);
+        }
+        // it's gotta be "click" or else you cannot stopPropagation() on it!
+        document.addEventListener('click', listener, true);
         return () => {
-            document.removeEventListener('mousedown', listener);
-            document.removeEventListener('touchstart', listener);
+            document.removeEventListener('click', listener, true);
         }
     }, [elem, handler])
 }
